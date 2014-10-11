dd7c1ab86660f14e40f30e1aae2a30b4ee85b06c
Thorsten Heymann <info@metashock.net>
2014-10-11 13:00:55 +0200

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'pacemaker'
+maintainer       'Thorsten Heymann'
+maintainer_email 'hek2mgl@metashock.net'
+license          'MIT'
+description      'Installs/Configures pacemaker'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
